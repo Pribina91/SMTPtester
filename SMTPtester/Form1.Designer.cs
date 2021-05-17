546a9857bca9f212743a23a9482749794e0cd4bb
Andrej Stajer <astajer@pribitech.com>
2021-05-17 18:10:22 +0200

    attachemnt testing

--- SMTPtester/Form1.Designer.cs
+++ SMTPtester/Form1.Designer.cs
@@ -45,97 +45,92 @@
             this.receiver = new System.Windows.Forms.TextBox();
             this.consolebox = new System.Windows.Forms.TextBox();
             this.clear = new System.Windows.Forms.Button();
+            this.hasAttachment = new System.Windows.Forms.CheckBox();
+            this.txtFilepath = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 69);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(34, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.Size = new System.Drawing.Size(77, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "SMTP server";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 223);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(34, 116);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Login";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(68, 283);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(34, 147);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.Size = new System.Drawing.Size(61, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Password";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(68, 329);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(34, 171);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 25);
+            this.label4.Size = new System.Drawing.Size(28, 15);
             this.label4.TabIndex = 3;
             this.label4.Text = "port";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(68, 388);
-            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Location = new System.Drawing.Point(34, 202);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 25);
+            this.label5.Size = new System.Drawing.Size(50, 15);
             this.label5.TabIndex = 4;
             this.label5.Text = "useSSL";
             // 
             // server
             // 
-            this.server.Location = new System.Drawing.Point(266, 56);
-            this.server.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.server.Location = new System.Drawing.Point(133, 29);
             this.server.Name = "server";
-            this.server.Size = new System.Drawing.Size(196, 31);
+            this.server.Size = new System.Drawing.Size(195, 20);
             this.server.TabIndex = 1;
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(266, 223);
-            this.login.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.login.Location = new System.Drawing.Point(133, 116);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(196, 31);
+            this.login.Size = new System.Drawing.Size(195, 20);
             this.login.TabIndex = 3;
             // 
             // password
             // 
             this.password.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
-            this.password.Location = new System.Drawing.Point(266, 277);
-            this.password.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.password.Location = new System.Drawing.Point(133, 144);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(196, 31);
+            this.password.Size = new System.Drawing.Size(195, 20);
             this.password.TabIndex = 4;
             // 
             // port
             // 
-            this.port.Location = new System.Drawing.Point(266, 327);
-            this.port.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.port.Location = new System.Drawing.Point(133, 171);
             this.port.Name = "port";
-            this.port.Size = new System.Drawing.Size(196, 31);
+            this.port.Size = new System.Drawing.Size(195, 20);
             this.port.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 498);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Location = new System.Drawing.Point(37, 368);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 44);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = true;
@@ -143,29 +138,26 @@
             // 
             // sender
             // 
-            this.sender.Location = new System.Drawing.Point(266, 106);
-            this.sender.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.sender.Location = new System.Drawing.Point(133, 55);
             this.sender.Name = "sender";
-            this.sender.Size = new System.Drawing.Size(196, 31);
+            this.sender.Size = new System.Drawing.Size(195, 20);
             this.sender.TabIndex = 2;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(68, 119);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Location = new System.Drawing.Point(34, 62);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.Size = new System.Drawing.Size(45, 15);
             this.label6.TabIndex = 8;
             this.label6.Text = "sender";
             // 
             // isSSLEnabled
             // 
             this.isSSLEnabled.AutoSize = true;
-            this.isSSLEnabled.Location = new System.Drawing.Point(266, 388);
-            this.isSSLEnabled.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.isSSLEnabled.Location = new System.Drawing.Point(133, 202);
             this.isSSLEnabled.Name = "isSSLEnabled";
-            this.isSSLEnabled.Size = new System.Drawing.Size(150, 29);
+            this.isSSLEnabled.Size = new System.Drawing.Size(89, 19);
             this.isSSLEnabled.TabIndex = 6;
             this.isSSLEnabled.Text = "checkBox1";
             this.isSSLEnabled.UseVisualStyleBackColor = true;
@@ -173,46 +165,92 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(74, 462);
-            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Location = new System.Drawing.Point(37, 240);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(30, 25);
+            this.label7.Size = new System.Drawing.Size(17, 15);
             this.label7.TabIndex = 10;
             this.label7.Text = "to";
             // 
             // receiver
             // 
-            this.receiver.Location = new System.Drawing.Point(266, 462);
-            this.receiver.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.receiver.Location = new System.Drawing.Point(133, 240);
             this.receiver.Name = "receiver";
-            this.receiver.Size = new System.Drawing.Size(196, 31);
+            this.receiver.Size = new System.Drawing.Size(195, 20);
             this.receiver.TabIndex = 7;
             // 
             // consolebox
             // 
-            this.consolebox.Location = new System.Drawing.Point(668, 56);
-            this.consolebox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.consolebox.Location = new System.Drawing.Point(334, 29);
             this.consolebox.Multiline = true;
             this.consolebox.Name = "consolebox";
-            this.consolebox.Size = new System.Drawing.Size(636, 635);
+            this.consolebox.Size = new System.Drawing.Size(320, 332);
             this.consolebox.TabIndex = 12;
             // 
             // clear
             // 
-            this.clear.Location = new System.Drawing.Point(1156, 708);
-            this.clear.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.clear.Location = new System.Drawing.Point(578, 368);
             this.clear.Name = "clear";
-            this.clear.Size = new System.Drawing.Size(150, 44);
+            this.clear.Size = new System.Drawing.Size(75, 23);
             this.clear.TabIndex = 13;
             this.clear.Text = "clear";
             this.clear.UseVisualStyleBackColor = true;
             this.clear.Click += new System.EventHandler(this.clear_Click);
             // 
+            // hasAttachment
+            // 
+            this.hasAttachment.AccessibleName = "hasAttachment";
+            this.hasAttachment.AutoSize = true;
+            this.hasAttachment.Location = new System.Drawing.Point(40, 267);
+            this.hasAttachment.Name = "hasAttachment";
+            this.hasAttachment.Size = new System.Drawing.Size(115, 19);
+            this.hasAttachment.TabIndex = 14;
+            this.hasAttachment.Text = "Has attachment";
+            this.hasAttachment.UseVisualStyleBackColor = true;
+            this.hasAttachment.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtFilepath
+            // 
+            this.txtFilepath.Location = new System.Drawing.Point(133, 292);
+            this.txtFilepath.Name = "txtFilepath";
+            this.txtFilepath.Size = new System.Drawing.Size(195, 20);
+            this.txtFilepath.TabIndex = 15;
+            this.txtFilepath.TextChanged += new System.EventHandler(this.txtFilepath_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(133, 318);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(195, 20);
+            this.txtName.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(37, 292);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 15);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Filepath";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(37, 318);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Name";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1406, 962);
+            this.ClientSize = new System.Drawing.Size(703, 500);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtFilepath);
+            this.Controls.Add(this.hasAttachment);
             this.Controls.Add(this.clear);
             this.Controls.Add(this.consolebox);
             this.Controls.Add(this.receiver);
@@ -230,7 +268,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -257,6 +294,11 @@
         private System.Windows.Forms.TextBox receiver;
         private System.Windows.Forms.TextBox consolebox;
         private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.CheckBox hasAttachment;
+        private System.Windows.Forms.TextBox txtFilepath;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- SMTPtester/Form1.cs
+++ SMTPtester/Form1.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
+using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace SMTPtester
@@ -13,6 +17,9 @@ namespace SMTPtester
         public Form1()
         {
             InitializeComponent();
+            
+            txtName.Enabled = false;
+            txtFilepath.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -21,34 +28,73 @@ namespace SMTPtester
 
             SmtpClient client = new SmtpClient();
 
-            client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-            client.Host = this.server.Text;
-            client.Port = int.Parse(this.port.Text);
-            client.Credentials = new System.Net.NetworkCredential(this.login.Text, this.password.Text);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Host = server.Text;
+            client.Port = int.Parse(port.Text);
+            client.Credentials = new NetworkCredential(login.Text, password.Text);
 
-            client.EnableSsl = this.isSSLEnabled.Checked;
-            client.Timeout = 5000;
+            client.EnableSsl = isSSLEnabled.Checked;
+            client.Timeout = 10 * 1000;
 
-            MailMessage message = new MailMessage(this.sender.Text, this.receiver.Text);
+            MailMessage message = new MailMessage(this.sender.Text, receiver.Text);
             message.Subject = "SMTP test";
             message.Body = "Test " + counter + " at " + DateTime.Now;
+
+
+            if (hasAttachment.Checked)
+            {
+                var attachment = loadAttachment(txtFilepath.Text,txtName.Text);
+                message.Attachments.Add(attachment);
+            }
 
             try
             {
                 client.Send(message);
-                this.consolebox.Text += counter + " sent success \n";
-                this.button1.BackColor = Color.ForestGreen;
+                consolebox.Text += counter + " sent success \n";
+                button1.BackColor = Color.ForestGreen;
             }
             catch (Exception ex)
             {
-                this.button1.BackColor = Color.Red;
-                this.consolebox.Text += counter + "sent fail\n" + ex.Message + ":" + ex.InnerException;
+                button1.BackColor = Color.Red;
+                consolebox.Text += counter + "sent fail\n" + ex.Message + ":" + ex.InnerException;
             }
         }
 
         private void clear_Click(object sender, EventArgs e)
         {
-            this.consolebox.Text = "";
+            consolebox.Text = "";
+        }
+
+        private Attachment loadAttachment(string fileName, string name)
+        {
+            var attachement = new Attachment(fileName);
+            attachement.Name = name;
+            return attachement;
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            var enabled = hasAttachment.Checked;
+            
+            {
+                txtName.Enabled = enabled;
+                txtFilepath.Enabled = enabled;
+            }
+        }
+
+        private void txtFilepath_TextChanged(object sender, EventArgs e)
+        {
+            var text = txtFilepath.Text;
+            if (System.IO.File.Exists(text))
+            {
+                txtFilepath.BackColor = Color.Green;
+            }
+            else
+            {
+                txtFilepath.BackColor = Color.Orange;
+            }
         }
     }
+
 }
